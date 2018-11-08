a6723efa3fd997de201d67914ff790775a162f78
lianyu.zou <lianyu.zou@qunar.com>
2018-11-09 07:36:17 +0800

    打包RN核心模块

---
+++ react-native-lib/RNCore/RNCore/RNCore.h
@@ -0,0 +1,13 @@
+//
+//  RNCore.h
+//  RNCore
+//
+//  Created by one on 2018/10/21.
+//  Copyright © 2018 ZLY.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNCore : NSObject
+
+@end
---
+++ react-native-lib/RNCore/RNCore/RNCore.m
@@ -0,0 +1,13 @@
+//
+//  RNCore.m
+//  RNCore
+//
+//  Created by one on 2018/10/21.
+//  Copyright © 2018 ZLY.com. All rights reserved.
+//
+
+#import "RNCore.h"
+
+@implementation RNCore
+
+@end
